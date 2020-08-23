e0f6f392d7242df48e21a79eb65f19f9bf2cfb32
Gregory Khvatsky <gregory.khvatsky@outlook.com>
2020-08-23 04:45:44 +0300

    Added an argument to control log verbosity

--- main.go
+++ main.go
@@ -19,12 +19,19 @@ var (
 	keyboardPath *string = flag.String("kbpath", "/sys/bus/hid/drivers/razerkbd/0003:1532:0228.0006", "Path to the directory with the keyboard file")
 	animFps      *int    = flag.Int("fps", -1, "Speed of the animation (will insert a delay of 1/fps seconds, -1 means no delay)")
 	animFile     *string = flag.String("anim", "./anim.apng", "Path to the animation file")
+	logLevel     *string = flag.String("loglevel", "info", "Log level, possible values: panic, fatal, error, warn/warning, info, debug, trace")
 )
 
 func main() {
-	logrus.SetLevel(logrus.DebugLevel)
-	logrus.Infoln("Parsing args")
 	flag.Parse()
+	logLevelP, err := logrus.ParseLevel(*logLevel)
+	if err != nil {
+		logLevelP = logrus.InfoLevel
+	}
+	logrus.SetLevel(logLevelP)
+	if err != nil {
+		logrus.Warnf("Failed to parse log level %s, defaulting to Info", *logLevel)
+	}
 
 	frames := make(chan image.Image)
 
